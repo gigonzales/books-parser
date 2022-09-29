cbd153ce4e807dbf507d862a57333bd949f1b285
Gemson Gonzales <gemson.gonzales@zenrooms.com>
2022-09-29 09:01:16 +0800

    Move json_encoded to file

--- bin/command.php
+++ bin/command.php
@@ -21,27 +21,43 @@ foreach ($files as $file) {
 if ($books > DATA_THRESHOLD) {
     $items = array_chunk(array_values(array_filter($books)), DATA_THRESHOLD);
 
+    $increment = 0;
     foreach ($items as $item) {
         $output = shell_exec('crontab -l');
+        $fileName = '/tmp/items-' . $increment .'.txt';
+        file_put_contents($fileName, json_encode($item));
+
         if (!str_contains(
                 $output,
                 '* * * * * cd ~/projects/books-parser && php bin/command.php '
-                . escapeshellarg(serialize($item)))
+                . $fileName)
         ) {
             file_put_contents(
                     '/tmp/crontab.txt',
                     $output . '* * * * * cd ~/projects/books-parser && php bin/command.php '
-                    . escapeshellarg(serialize($item))
-                    .' >> ../log.txt 2>&1'
+                    . $fileName
+                    .' >> /tmp/log.txt 2>&1'
                     . PHP_EOL
             );
             echo exec('crontab /tmp/crontab.txt');
         }
-
+        $increment++;
         print_r($item);
     }
+} else {
+    $output = shell_exec('crontab -l');
+    file_put_contents('/tmp/items', json_encode($books));
+    file_put_contents(
+        '/tmp/crontab.txt',
+        $output . '* * * * * cd ~/projects/books-parser && php bin/command.php '
+        . '/tmp/items.txt'
+        .' >> /tmp/log.txt 2>&1'
+        . PHP_EOL
+    );
+    echo exec('crontab /tmp/crontab.txt');
 }
 
 if (isset($argv[1])) {
-    $command->insertOrUpdate(unserialize($argv[1]));
+    $data = file_get_contents($argv[1]);
+    $command->insertOrUpdate(json_decode($data, true));
 }
